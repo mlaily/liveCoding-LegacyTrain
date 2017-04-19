f9e40cbd303847289a4d6a4c6bd8fda811f85bba
Thomas PIERRAIN <thomas@pierrain.net>
2017-04-19 11:14:02 +0200

    Introduce Seat and Train types within the TrainDataService

---
+++ AcmeCorp.TrainDataService/Models/Seat.cs
@@ -0,0 +1,16 @@
+﻿namespace AcmeCorp.TrainDataService.Models
+{
+    public class Seat
+    {
+        public string booking_reference { get; set; }
+        public string seat_number { get; set; }
+        public string coach { get; set; }
+
+        public Seat(string coach, string seatNumber, string bookingReference)
+        {
+            booking_reference = bookingReference;
+            seat_number = seatNumber;
+            this.coach = coach;
+        }
+    }
+}
--- AcmeCorp.TrainDataService/Models/Train.cs
+++ AcmeCorp.TrainDataService/Models/Train.cs
@@ -1,16 +1,28 @@
-﻿namespace AcmeCorp.TrainDataService.Models
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace AcmeCorp.TrainDataService.Models
 {
     public class Train
     {
         private string trainId;
+        private readonly List<Seat> _seats;
+
 
         public Train(string trainId)
         {
             this.trainId = trainId;
+            _seats = new List<Seat>();
+            _seats.Add(new Seat("A", "43", ""));
+            _seats.Add(new Seat("A", "44", "45klksnFaZ"));
         }
+
+        public IEnumerable<Seat> seats => _seats;
 
         public override string ToString()
         {
+            return JsonConvert.SerializeObject(this);
+
             return
                 "{\"seats\": {\"1A\": {\"booking_reference\": \"\", \"seat_number\": \"1\", \"coach\": \"A\"}, \"2A\": {\"booking_reference\": \"\", \"seat_number\": \"2\", \"coach\": \"A\"}}}";
         }
