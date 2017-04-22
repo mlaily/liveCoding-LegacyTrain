239c8c2e5e5d74139d220bd9c0ea9dfbd986e0e7
Thomas PIERRAIN <thomas@pierrain.net>
2017-04-22 19:09:24 +0200

    Clean the ReserveController of the TrainDataService and add comments explaining how to PUT a reservation request

--- AcmeCorp.TrainDataService/Controllers/ReserveController.cs
+++ AcmeCorp.TrainDataService/Controllers/ReserveController.cs
@@ -4,9 +4,17 @@ using Microsoft.AspNetCore.Mvc;
 namespace AcmeCorp.TrainDataService.Controllers
 {
     [Route("api/reserve")]
-    //[Route("api/[controller]")]
     public class ReserveController : Controller
     {
+        // PUT on 
+        // http://localhost:50680/api/reserve
+        // with JSON payload:
+        //  {
+        //      "train_id": "5FSdR",
+        //      "seats": ["4A", "5A"],
+        //      "booking_reference": "Td98kms"
+        //  }
+        //
         private readonly IProvideTrain trainProvider;
 
         public ReserveController(IProvideTrain trainProvider)
@@ -14,38 +22,23 @@ namespace AcmeCorp.TrainDataService.Controllers
             this.trainProvider = trainProvider;
         }
 
-        // GET api/data_for_train
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
-
-        // GET api/data_for_train/5FSdR
-        [HttpGet("{trainId}")]
-        //[HttpGet]
-        public string Get(string trainId)
-        {
-            return this.trainProvider.GetTrain(trainId).ToString();
-        }
-
         // POST api/data_for_train
         [HttpPost]
-        public void Post([FromBody]TrainUpdateDTO value)
+        public void Post([FromBody] TrainUpdateDTO value)
         {
-            this.trainProvider.UpdateTrainReservations(value);
+            trainProvider.UpdateTrainReservations(value);
         }
 
-        // PUT api/data_for_train/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT api/data_for_train/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE api/data_for_train/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/data_for_train/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
