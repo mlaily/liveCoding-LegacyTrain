fc9ebe0211bd915221c51ef9e594bd59fed4b28a
Bruno Boucard <boucard.bruno@free.fr>
2017-06-07 14:58:09 +0200

    code covered

--- TrainTrain.Test/Acceptance/TrainTrainTests.cs
+++ TrainTrain.Test/Acceptance/TrainTrainTests.cs
@@ -1,6 +1,67 @@
-﻿namespace TrainTrain.Test.Acceptance
+﻿using System.Threading.Tasks;
+using NFluent;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace TrainTrain.Test.Acceptance
 {
     public class TrainTrainTests
     {
+        [Test]
+        public void Should_reserve_seats_when_train_is_empty()
+        {
+            const string trainId = "9043-2017-06-07";
+            const string bookingReference = "75bcd15";
+
+            var trainDataService = BuildTrainDataService(trainId, TrainTopologyGenerator.GetTrainWith10AvailableSeats());
+            var bookingReferenceService = BuildBookingReferenceService(bookingReference);
+
+            var webTicketManager = new WebTicketManager(trainDataService, bookingReferenceService);
+            var reservation = webTicketManager.Reserve(trainId, 3).Result;
+            Check.That(reservation).IsEqualTo("{\"train_id\": \"9043-2017-06-07\", \"booking_reference\": \"75bcd15\", \"seats\": [\"1A\", \"2A\", \"3A\"]}");
+        }
+
+        [Test]
+        public void Should_not_reserve_seats_when_it_exceed_max_capacty_threshold()
+        {
+            const string trainId = "9043-2017-06-07";
+            const string bookingReference = "75bcd15";
+
+            var trainDataService = BuildTrainDataService(trainId, TrainTopologyGenerator.GetTrainWith10ASeatsAnd6AlreadyReserved());
+            var bookingReferenceService = BuildBookingReferenceService(bookingReference);
+
+            var webTicketManager = new WebTicketManager(trainDataService, bookingReferenceService);
+            var reservation = webTicketManager.Reserve(trainId, 3).Result;
+            Check.That(reservation).IsEqualTo("{\"train_id\": \"9043-2017-06-07\", \"booking_reference\": \"\", \"seats\": []}");
+        }
+
+        [Test]
+        public void Should_reserve_all_seats_in_the_same_coach()
+        {
+            const string trainId = "9043-2017-06-07";
+            const string bookingReference = "75bcd15";
+
+            var trainDataService = BuildTrainDataService(trainId, TrainTopologyGenerator.GetTrainWith2CoachesAnd9SeatsAlreadyReservedInTheFirstCoach());
+            var bookingReferenceService = BuildBookingReferenceService(bookingReference);
+
+            var webTicketManager = new WebTicketManager(trainDataService, bookingReferenceService);
+            var reservation = webTicketManager.Reserve(trainId, 2).Result;
+            Check.That(reservation).IsEqualTo("{\"train_id\": \"9043-2017-06-07\", \"booking_reference\": \"75bcd15\", \"seats\": [\"1B\", \"2B\"]}");
+        }
+
+        private static IBookingReferenceService BuildBookingReferenceService(string bookingReference)
+        {
+            var bookingReferenceService = Substitute.For<IBookingReferenceService>();
+            bookingReferenceService.GetBookingReference().Returns(Task.FromResult(bookingReference));
+            return bookingReferenceService;
+        }
+
+        private static ITrainDataService BuildTrainDataService(string trainId, string trainTopology)
+        {
+            var trainDataService = Substitute.For<ITrainDataService>();
+            trainDataService.GetTrain(trainId)
+                .Returns(Task.FromResult(trainTopology));
+            return trainDataService;
+        }
     }
 }
--- TrainTrain/WebTicketManager.cs
+++ TrainTrain/WebTicketManager.cs
@@ -4,35 +4,41 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using TrainTrain.Dal;
-using TrainTrain.Dal.Entities;
 
 namespace TrainTrain
 {
     public class WebTicketManager
     {
-        private const string uriBookingReferenceService = "http://localhost:51691/";
-        private const string urITrainDataService = "http://localhost:50680";
-        private ITrainCaching _trainCaching;
-        public WebTicketManager()
+        private const string UriBookingReferenceService = "http://localhost:51691/";
+        private const string UriTrainDataService = "http://localhost:50680";
+        private readonly ITrainCaching _trainCaching;
+        private readonly ITrainDataService _trainDataService;
+        private readonly IBookingReferenceService _bookingReferenceService;
+
+        public WebTicketManager():this(new TrainDataService(UriTrainDataService), new BookingReferenceService(UriBookingReferenceService))
         {
+
+        }
+
+        public WebTicketManager(ITrainDataService trainDataService, IBookingReferenceService bookingReferenceService)
+        {
+
+            _trainDataService = trainDataService;
+            _bookingReferenceService = bookingReferenceService;
             _trainCaching = new TrainCaching();
             _trainCaching.Clear();
+    
         }
-        public async Task<string> Reserve(string train, int seats)
+        public async Task<string> Reserve(string trainId, int seatsRequestedCount)
         {
             List<Seat> availableSeats = new List<Seat>();
             int count = 0;
-            var result = string.Empty;
-            string bookingRef;
 
             // get the train
-            var JsonTrain = await GetTrain(train);
+            var jsonTrain = await _trainDataService.GetTrain(trainId);
 
-            result = JsonTrain;
-
-            var trainInst = new Train(JsonTrain);
-            if ((trainInst.ReservedSeats + seats) <= Math.Floor(ThreasholdManager.GetMaxRes() * trainInst.GetMaxSeat()))
+            var trainInst = new Train(jsonTrain);
+            if (trainInst.ReservedSeats + seatsRequestedCount <= Math.Floor(ThreasholdManager.GetMaxRes() * trainInst.GetMaxSeat()))
             {
                 var numberOfReserv = 0;
                 // find seats to reserve
@@ -42,33 +48,28 @@ namespace TrainTrain
                     if (each.BookingRef == "")
                     {
                         i++;
-                        if (i <= seats)
+                        if (i <= seatsRequestedCount)
                         {
                             availableSeats.Add(each);
                         }
                     }
                 }
 
-                foreach (var a in availableSeats)
+                foreach (var unused in availableSeats)
                 {
                     count++;
                 }
 
                 var reservedSets = 0;
 
-
-                if (count != seats)
+                string bookingRef;
+                if (count != seatsRequestedCount)
                 {
-                    return string.Format("{{\"train_id\": \"{0}\", \"booking_reference\": \"\", \"seats\": []}}",
-                        train);
+                    return $"{{\"train_id\": \"{trainId}\", \"booking_reference\": \"\", \"seats\": []}}";
                 }
                 else
                 {
-
-                    using (var client = new HttpClient())
-                    {
-                        bookingRef = await GetBookRef(client);
-                    }
+                    bookingRef = await _bookingReferenceService.GetBookingReference();
 
                     foreach (var availableSeat in availableSeats)
                     {
@@ -78,41 +79,17 @@ namespace TrainTrain
                     }
                 }
 
-                if (numberOfReserv == seats)
+                if (numberOfReserv == seatsRequestedCount)
                 {
-                    await _trainCaching.Save(train, trainInst, bookingRef);
+                    await _trainCaching.Save(trainId, trainInst, bookingRef);
 
-                    using (var client = new HttpClient())
-                    {
-                        var value = new MediaTypeWithQualityHeaderValue("application/json");
-                        client.BaseAddress = new Uri(urITrainDataService);
-                        client.DefaultRequestHeaders.Accept.Clear();
-                        client.DefaultRequestHeaders.Accept.Add(value);
-
-                        if (reservedSets == 0)
-                        {
-                            Console.WriteLine("Reserved seat(s): ", reservedSets);
-                        }
-
-                        // HTTP POST
-                        HttpContent resJSON = new StringContent(buildPostContent(train, bookingRef, availableSeats), Encoding.UTF8, "application/json");
-                        var response = await client.PostAsync("reserve", resJSON);
-
-                        response.EnsureSuccessStatusCode();
-
-                        var todod = "[TODOD]";
-
-
-                        return string.Format(
-                            "{{\"train_id\": \"{0}\", \"booking_reference\": \"{1}\", \"seats\": {2}}}", 
-                            train,
-                            bookingRef, 
-                            dumpSeats(availableSeats));
-                    }
+                    await _trainDataService.ReserveSeats(trainId, bookingRef, availableSeats);
+                    return
+                            $"{{\"train_id\": \"{trainId}\", \"booking_reference\": \"{bookingRef}\", \"seats\": {dumpSeats(availableSeats)}}}";
+                    
                 }
             }
-
-            return string.Format("{{\"train_id\": \"{0}\", \"booking_reference\": \"\", \"seats\": []}}", train);
+            return $"{{\"train_id\": \"{trainId}\", \"booking_reference\": \"\", \"seats\": []}}";
         }
 
         private string dumpSeats(IEnumerable<Seat> seats)
@@ -137,66 +114,6 @@ namespace TrainTrain
             sb.Append("]");
 
             return sb.ToString();
-        }
-
-        private static string buildPostContent(string trainId, string bookingRef, IEnumerable<Seat> availableSeats)
-        {
-            var seats = new StringBuilder("[");
-            bool firstTime = true;
-
-            foreach (var s in availableSeats)
-            {
-                if (!firstTime)
-                {
-                    seats.Append(", ");
-                }
-                else
-                {
-                    firstTime = false;
-                }
-
-                seats.Append(string.Format("\"{0}{1}\"", s.SeatNumber, s.CoachName));
-            }
-            seats.Append("]");
-
-            var result = string.Format(
-                "{{\r\n\t\"train_id\": \"{0}\",\r\n\t\"seats\": {1},\r\n\t\"booking_reference\": \"{2}\"\r\n}}",
-                trainId, seats.ToString(), bookingRef);
-
-            return result;
-        }
-
-        protected async Task<string> GetTrain(string train)
-        {
-            string JsonTrainTopology;
-            using (var client = new HttpClient())
-            {
-                var value = new MediaTypeWithQualityHeaderValue("application/json");
-                client.BaseAddress = new Uri(urITrainDataService);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(value);
-
-                // HTTP GET
-                var response = await client.GetAsync(string.Format("api/data_for_train/{0}", train));
-                response.EnsureSuccessStatusCode();
-                JsonTrainTopology = await response.Content.ReadAsStringAsync();
-            }
-            return JsonTrainTopology;
-        }
-
-        protected async Task<string> GetBookRef(HttpClient client)
-        {
-            var value = new MediaTypeWithQualityHeaderValue("application/json");
-            client.BaseAddress = new Uri(uriBookingReferenceService);
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(value);
-
-            // HTTP GET
-            var response = await client.GetAsync("/booking_reference");
-            response.EnsureSuccessStatusCode();
-
-            var bookingRef = await response.Content.ReadAsStringAsync();
-            return bookingRef;
         }
     }
 }
