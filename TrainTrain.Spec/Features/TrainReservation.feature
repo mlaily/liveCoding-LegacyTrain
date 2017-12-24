6867030b298b6bb5321bc3e408ea2736f9678067
Bruno Boucard <boucard.bruno@free.fr>
2017-12-24 13:39:57 +0400

    add specification with examples

---
+++ TrainTrain.Spec/Features/TrainReservation.feature
@@ -0,0 +1,26 @@
+﻿Feature: TrainTrain Reservation
+	As a French start-up, TrainTrain Company is aimed at helping passengers 
+	to book seats in national trains smoothly.
+
+	On the other hand, we are looking for user satisfaction	
+	to become a leader in our market.
+
+Background: 
+	Given the train "9043-2018-04-17"
+
+Scenario: For a train overall, no more than 70% of seats may be reserved in advance
+	Given 1 coaches of 10 seats available
+	When 3 seats are requested
+	Then the reservation 75bcd15 should be assigned these seats "1A, 2A, 3A"
+
+Scenario: Not reserve seats when it exceeds train max capacity 70%
+	Given 1 coaches of 10 seats and 6 already reserved
+	When 3 seats are requested
+	Then the reservation 75bcd15 should be failed
+
+@ignore @while refactoring
+Scenario: Each reservation must be booked in the same coach
+	Given 2 coaches of 10 seats and 9 seats already reserved in the coach 1
+	When 2 seats are requested
+	Then the reservation 75bcd15 should be assigned these seats "1B, 2B" in the same coach
+
---
+++ TrainTrain.Spec/StepDefinitions/TrainTrainReservationSteps.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NFluent;
+using NSubstitute;
+using TechTalk.SpecFlow;
+using TrainTrain.Test.Acceptance;
+
+namespace TrainTrain.Spec.StepDefinitions
+{
+    [Binding]
+    public class TrainTrainReservationSteps
+    {
+        private const string ArbitraryBookingReference = "XYZ0123";
+        private string _trainId;
+        private int _seatsRequestedCount;
+
+        private string _trainTopology;
+
+        [Given(@"the train ""(.*)""")]
+        public void GivenTheTrain(string trainId)
+        {
+            _trainId = trainId;
+        }
+
+        [Given(@"(.*) coaches of (.*) seats available")]
+        public void GivenTheTrainContainsCoachesOfSeatsAvailables(int coachCount, int seatsCount)
+        {
+
+            _trainTopology = TrainTopologyGenerator.Generate(coachCount, seatsCount);
+        }
+
+        [When(@"(.*) seats are requested")]
+        public void WhenSeatsRequested(int seatsRequestedCount)
+        {
+            _seatsRequestedCount = seatsRequestedCount;
+        }
+
+        [Given(@"(.*) coaches of (.*) seats and (.*) already reserved")]
+        public void GivenCoachesOfSeatsWithAlreadyReserved(int coachesCount, int availableSeatsCount,
+            int seatsReservedCount)
+        {
+            _trainTopology =
+                TrainTopologyGenerator.Generate(coachesCount, availableSeatsCount, ArbitraryBookingReference, seatsReservedCount);
+        }
+
+        [Given(@"(.*) coaches of (.*) seats and (.*) seats already reserved in the coach (.*)")]
+        public void GivenCoachesOfSeatsWithSeatsAlreadyReservedInTheCoach(int coachesCount, int availableSeatsCount, int seatsReservedCount, int coachNumberWhereReserved)
+        {
+            _trainTopology =
+                TrainTopologyGenerator.Generate(coachesCount, availableSeatsCount, ArbitraryBookingReference, seatsReservedCount, coachNumberWhereReserved);
+        }
+
+        [Then(@"the reservation (.*) should be assigned these seats ""(.*)""")]
+        public void ThenTheReservationShouldReserveSeats(string bookingReference, string expectedSeats)
+        {
+            var trainDataService = BuildTrainDataService(_trainId, _trainTopology);
+            var bookingReferenceService = BuildBookingReferenceService(bookingReference);
+
+            var webTicketManager = new WebTicketManager(trainDataService, bookingReferenceService);
+            var jsonReservation = webTicketManager.Reserve(_trainId, _seatsRequestedCount).Result;
+
+            Check.That(jsonReservation)
+                .IsEqualTo($"{{\"train_id\": \"{_trainId}\", \"booking_reference\": \"{bookingReference}\", \"seats\": [{AdaptSeatsInJson(expectedSeats)}]}}");         
+        }
+
+        [Then(@"the reservation (.*) should be failed")]
+        public void ThenTheReservationShouldFail(string bookingReference)
+        {
+            var trainDataService = BuildTrainDataService(_trainId, _trainTopology);
+            var bookingReferenceService = BuildBookingReferenceService(bookingReference);
+
+            var webTicketManager = new WebTicketManager(trainDataService, bookingReferenceService);
+            var jsonReservation = webTicketManager.Reserve(_trainId, _seatsRequestedCount).Result;
+
+            Check.That(jsonReservation)
+                .IsEqualTo($"{{\"train_id\": \"{_trainId}\", \"booking_reference\": \"\", \"seats\": []}}");
+        }
+        
+        [Then(@"the reservation (.*) should be assigned these seats ""(.*)"" in the same coach")]
+        public void ThenTheReservationBcdShouldReserveInSameCoach(string bookingReference, string expectedSeats)
+        {
+            var trainDataService = BuildTrainDataService(_trainId, _trainTopology);
+            var bookingReferenceService = BuildBookingReferenceService(bookingReference);
+
+            var webTicketManager = new WebTicketManager(trainDataService, bookingReferenceService);
+            var jsonReservation = webTicketManager.Reserve(_trainId, _seatsRequestedCount).Result;
+
+            Check.That(jsonReservation)
+                .IsEqualTo($"{{\"train_id\": \"{_trainId}\", \"booking_reference\": \"{bookingReference}\", \"seats\": [{AdaptSeatsInJson(expectedSeats)}]}}");
+        }
+
+        private static string AdaptSeatsInJson(string expectedSeats)
+        {
+            var expectedSeatsSplited = expectedSeats.Split(',').Select(x => x.Trim());
+            return "\"" + string.Join("\", \"", expectedSeatsSplited) + "\"";
+        }
+
+        private static IBookingReferenceService BuildBookingReferenceService(string bookingReference)
+        {
+            var bookingReferenceService = Substitute.For<IBookingReferenceService>();
+            bookingReferenceService.GetBookingReference().Returns(Task.FromResult(bookingReference));
+            return bookingReferenceService;
+        }
+
+        private static ITrainDataService BuildTrainDataService(string trainId, string trainTopology)
+        {
+            var trainDataService = Substitute.For<ITrainDataService>();
+            trainDataService.GetTrain(trainId)
+                .Returns(Task.FromResult(new Train(TrainDataService.AdaptTrainTopology(trainTopology))));
+            return trainDataService;
+        }
+    }
+}
